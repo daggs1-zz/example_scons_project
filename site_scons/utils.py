1f9dc68a082ecd240dc93061a6582fc87195dc9d
Dagg Stompler <daggs@gmx.com>
2019-09-03 23:29:23 +0300

    utils.py: migrate to pkg based handling
    instead of file name, pass pkg name and search for the latest version in
    the src folder for it.
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- site_scons/utils.py
+++ site_scons/utils.py
@@ -1,12 +1,12 @@
 from SCons.Script import *
-import os
+import os, glob
  
-unpack = Builder(action=[ "@rm -rf ${TARGET.dir}", "@tar xf ${SOURCE.abspath} -C ${TARGET.dir.dir}", "@touch ${TARGET.abspath}" ])#, target_factory=Dir)
+unpack = Builder(action=[ "@echo unpacking `echo ${SOURCE.filebase} | sed 's/\.[^.]*$//;s/-/ \(version /g'`\)...", "@rm -rf ${TARGET.dir}", "@tar xf ${SOURCE.abspath} -C ${TARGET.dir.dir}", "@touch ${TARGET.abspath}" ])#, target_factory=Dir)
 # util_env = Environment(BUILDERS = { "unpack": unpack, })
 
-def unpack_src(env, tar_file):
+def unpack_src(env, pkg_name):
 	dst_path = env['O']
-	src_tar_path = env['SRCS_FOLDER'] + '/' + tar_file
+	src_tar_path = sorted(glob.glob(Dir(env['SRCS_FOLDER']).abspath + '/' + pkg_name + '*'))[-1]
 	tar_file_path = File(src_tar_path).abspath
 	if not os.path.isfile(tar_file_path):
 		raise SCons.Errors.UserError, "%s file was not found." %tar_file_path
@@ -16,7 +16,6 @@ def unpack_src(env, tar_file):
 	if not len(folder):
 		raise SCons.Errors.UserError, "%s is invalid." %tar_file_path
 
-	print("unpacking " + tar_file + "...")
 	r = env.unpack(dst_path + '/' + folder + '.unpacked', src_tar_path)
 	if not r:
 		raise SCons.Errors.UserError, "failed to extract %s." %tar_file_path
