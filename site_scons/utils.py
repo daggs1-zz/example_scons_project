1a116ad6de5c10fbd381b8d50ac189f9e1fd030b
Dagg Stompler <daggs@gmx.com>
2019-09-03 22:45:01 +0300

    utils.py: prevent constant unpack
    previous implementation resulted in constant unpacking whenever scons
    was called.
    now it won't unpack if .unpacked file exists.
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- site_scons/utils.py
+++ site_scons/utils.py
@@ -1,14 +1,19 @@
 from SCons.Script import *
 import os
  
-unpack = Builder(action="tar xf ${SOURCE.abspath} -C ${TARGET.dir}")#, target_factory=Dir)
+unpack = Builder(action=[ "rm -rf ${TARGET.dir}", "tar xf ${SOURCE.abspath} -C ${TARGET.dir.dir}", "touch ${TARGET.abspath}" ])#, target_factory=Dir)
 # util_env = Environment(BUILDERS = { "unpack": unpack, })
- 
+
 def unpack_src(env, dst_path, tar_file):
-    tar_file_path= File(tar_file).abspath
-    if not os.path.isfile(tar_file_path):
-        raise SCons.Errors.UserError, "%s file was not found." %tar_file_path
- 
-    r = env.unpack(dst_path, tar_file)
-    if not r:
-        raise SCons.Errors.UserError, "failed to extract %s." %tar_file_path
+	tar_file_path = File(tar_file).abspath
+	if not os.path.isfile(tar_file_path):
+		raise SCons.Errors.UserError, "%s file was not found." %tar_file_path
+
+	folder = os.popen("tar tvf " + tar_file_path + " 2>/dev/null | head -1 | awk '{print $NF}' | tr -d '\n'").read()
+
+	if not len(folder):
+		raise SCons.Errors.UserError, "%s is invalid." %tar_file_path
+
+	r = env.unpack(dst_path + '/' + folder + '.unpacked', tar_file)
+	if not r:
+		raise SCons.Errors.UserError, "failed to extract %s." %tar_file_path
