aaa0e0fd86f7e7e279c840e0758b76ea27a1b57f
Dagg Stompler <daggs@gmx.com>
2019-09-07 15:44:24 +0300

    scons: add support for stages based flow
    as title says, this wil allow more rebust approach to the pkg handling
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- SConstruct
+++ SConstruct
@@ -3,16 +3,14 @@ import utils
 
 # define std pathes
 path = ['/usr/local/sbin', '/usr/local/bin',
-        '/usr/sbin', '/usr/bin', '/sbin', '/bin']
+	'/usr/sbin', '/usr/bin', '/sbin', '/bin']
 
 env = Environment(T="#/output/$flavor/binaries",
-                  O="#/output/$flavor/objects",
-                  ENV={'PATH': path},
-                  flavor="debug",
-                  ROOT='#',
-                  SRCS_FOLDER='#/src')
-
-env.Append(BUILDERS = {'unpack':utils.unpack})
+		  O="#/output/$flavor/objects",
+		  ENV={'PATH': path},
+		  flavor="debug",
+		  ROOT='#',
+		  SRCS_FOLDER='#/src')
 
 SConscript('src/SConscript', variant_dir="$O",
-           duplicate=0, exports='env')
+	   duplicate=0, exports='env')
--- site_scons/utils.py
+++ site_scons/utils.py
@@ -1,21 +1,41 @@
 from SCons.Script import *
 import os, glob
  
-unpack = Builder(action=[ "@echo unpacking `echo ${SOURCE.filebase} | sed 's/\.[^.]*$//;s/-/ \(version /g'`\)...", "@rm -rf ${TARGET.dir}", "@tar xf ${SOURCE.abspath} -C ${TARGET.dir.dir}", "@touch ${TARGET.abspath}" ])#, target_factory=Dir)
-# util_env = Environment(BUILDERS = { "unpack": unpack, })
+src_unpack = Builder(action=[ "@echo unpacking `echo ${SOURCE.filebase} | sed 's/\.[^.]*$//;s/-/ \(version /g'`\)...", "@rm -rf ${TARGET}", "@tar xf ${SOURCE.abspath} -C ${TARGET.dir}", "mkdir ${TARGET}/.scons_build_stages" ])
 
-def unpack_src(env, pkg_name):
-	dst_path = env['O']
-	src_tar_path = sorted(glob.glob(Dir(env['SRCS_FOLDER']).abspath + '/' + pkg_name + '*'))[-1]
+
+def get_stages_folder_name():
+	return '.scons_build_stages'
+
+def prepare_env(env, pkg_name):
+	src_tar_path = sorted(glob.glob(Dir(env['SRCS_FOLDER']).abspath + '/' + pkg_name + '-*'))[-1]
 	tar_file_path = File(src_tar_path).abspath
 	if not os.path.isfile(tar_file_path):
 		raise SCons.Errors.UserError, "%s file was not found." %tar_file_path
-
+	
 	folder = os.popen("tar tvf " + tar_file_path + " 2>/dev/null | head -1 | awk '{print $NF}' | tr -d '\n'").read()
-
+	
 	if not len(folder):
 		raise SCons.Errors.UserError, "%s is invalid." %tar_file_path
+	env['unpacked_path'] = env.subst('$O') + '/' + folder
+	env['src_tar_path'] = src_tar_path
 
-	r = env.unpack(dst_path + '/' + folder + '.unpacked', src_tar_path)
+def unpack(env, pkg_name):
+	r = env.unpack(Dir(env['unpacked_path']), File(env['src_tar_path']))
 	if not r:
-		raise SCons.Errors.UserError, "failed to extract %s." %tar_file_path
+		raise SCons.Errors.UserError, "failed to extract %s." %File(env['src_tar_path']).abspath
+
+def patch(env, pkg_name):
+	raise SCons.Errors.UserError, "unimplemented"
+
+def prepare(env, pkg_name):
+	raise SCons.Errors.UserError, "unimplemented"
+
+def compile(env, pkg_name):
+	raise SCons.Errors.UserError, "unimplemented"
+
+def install(env, pkg_name):
+	raise SCons.Errors.UserError, "unimplemented"
+
+def init_utils_env(env):
+    return env.Clone(BUILDERS = {'unpack' : src_unpack})
