a3f6c79113c1fdca1f825743e5e775b55ab6ee86
Dagg Stompler <daggs@gmx.com>
2019-09-17 17:38:27 +0300

    Make.py: handle build type
    in preperation for the bug refractor, handle build type as part of the
    make, if install flow, the build type defines the behavior of destdir
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- site_scons/site_tools/Make.py
+++ site_scons/site_tools/Make.py
@@ -1,7 +1,7 @@
 # make builder
 
 from pathlib2 import Path
-import subprocess
+import subprocess, build_type
 from SCons.Script import *
 
 def parse_args(target, source, env):
@@ -12,6 +12,7 @@ def parse_args(target, source, env):
 	implicit = False
 	install_flow = False
 	make_targets = ""
+	b_type = build_type.type.UNSUPPORTED
 
 	if 'single_job' in env.Dictionary().keys() and env['single_job'] == True:
 		jobs = 1
@@ -22,15 +23,16 @@ def parse_args(target, source, env):
 	if 'install' in env.Dictionary().keys():
 		 install_flow = env['install']
 		 dest_dir = env.subst(str(env['dest_dir']))
+		 b_type = env.subst(env['build_type'])
 		 implicit = False
 
 	if 'make_targets' in env.Dictionary().keys():
 		make_targets = env.subst(str(env['make_targets']))
 
-	return (pkg_name, work_dir, dest_dir, jobs, implicit, install_flow, make_targets)
+	return (pkg_name, work_dir, dest_dir, jobs, implicit, install_flow, make_targets, b_type)
 
 def message(target, source, env):
-	(pkg_name, work_dir, dest_dir, jobs, implicit, install_flow, make_targets) = parse_args(target, source, env)
+	(pkg_name, work_dir, dest_dir, jobs, implicit, install_flow, make_targets, b_type) = parse_args(target, source, env)
 	msg = ""
 
 	if not implicit:
@@ -51,12 +53,15 @@ def message(target, source, env):
 	return msg
 
 def builder(target, source, env):
-	(pkg_name, work_dir, dest_dir, jobs, implicit, install_flow, make_targets) = parse_args(target, source, env)
+	(pkg_name, work_dir, dest_dir, jobs, implicit, install_flow, make_targets, b_type) = parse_args(target, source, env)
 
 	cmd = 'make -j ' + str(jobs)
 
 	if install_flow:
-		cmd += ' DESTDIR=\"' + dest_dir + '\" install'
+		print(b_type)
+		if b_type != build_type.type.CONFIGURE:
+			cmd += ' DESTDIR=\"' + dest_dir + '\"'
+		cmd += ' install'
 
 	if len(make_targets) > 0:
 		cmd += ' ' + make_targets
